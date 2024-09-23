f0fb87bba306667946deea7083ab107df200f7d7
184932 <184932@stud.hvl.no>
2024-09-23 18:52:08 +0200

    Update Tabeller.java

--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -4,32 +4,43 @@ public class Tabeller {
 
 	// a)
 	public static void skrivUt(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden skrivUt ikke implementert");
-
+		
+		for ( int tall : tabell) {
+			System.out.print(tall + " ");
+		}
 	}
 
 	// b)
 	public static String tilStreng(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
+		StringBuilder sb = new StringBuilder ("[");
+		for (int i = 0; i < tabell.length; i++) {
+			sb.append(tabell[i]);
+			if (i < tabell.length - 1) {
+				sb.append(",");
+			}
+		}
+		sb.append("]");
+		return sb.toString();
 	}
 
 	// c)
 	public static int summer(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden summer ikke implementert");
+		int sum = 0;
+		for (int tall : tabell) {
+			sum += tall;
+		}
+		return sum;
 	}
 
 	// d)
 	public static boolean finnesTall(int[] tabell, int tall) {
-
-		// TODO
-		throw new UnsupportedOperationException("Metoden finnesTall ikke implementert");
-
+		for (int tabelltall : tabell) {
+			if (tabelltall == tall) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	// e)
